eadf161095b9c0748f241676b19628018f6383ec
Brad Wise <brad@bradandmarsha.com>
2024-02-05 19:34:31 -0600

    Terraform code to create a large number of SQS queues

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.35"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+locals {
+  seller_ids = jsondecode(file("./seller_ids.json"))
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  for_each = toset(local.seller_ids)
+  name                      = "sp-api-seller-${each.key}"
+}
